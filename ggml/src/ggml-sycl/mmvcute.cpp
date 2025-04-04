974848e11c69ec4f2c1766ee578a141e1f255962
Alberto Cabrera <alberto.cabrera@codeplay.com>
2025-04-04 20:36:04 +0100

    wip : Reordered working Q4_0 ocl (no blockload yet)

--- ggml/src/ggml-sycl/mmvcute.cpp
+++ ggml/src/ggml-sycl/mmvcute.cpp
@@ -7,10 +7,10 @@
 
 #include "mmvcute.hpp"
 
+#include <cstdint>
 #include <cstdio>
 
 #include "dpct/helper.hpp"
-#include "sycl/group_algorithm.hpp"
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
@@ -27,19 +27,6 @@
 #include <cute/util/debug.hpp>
 
 #pragma clang diagnostic pop
-
-#ifdef __SYCL_DEVICE_ONLY__
-template <class T, int N> using vector_t = T __attribute__((ext_vector_type(N)));
-#else
-template <class T, int N> using vector_t = sycl::marray<T, N>;
-#endif
-
-#ifdef __SYCL_DEVICE_ONLY__
-namespace detail {
-SYCL_EXTERNAL extern "C" vector_t<ushort, 2> __builtin_IB_subgroup_block_read_flat_u8_m1k32v2(
-    long baseoffset, int width_minus_one, int height_minus_one, int pitch_minus_one, vector_t<int, 2> coord);
-}
-#endif
 
 // TODO: Figure this out
 static __dpct_inline__ int get_int_from_uint8(const uint8_t * x8, const int & i32) {
@@ -63,64 +50,84 @@ constexpr size_t safe_div(const size_t m, const size_t n) {
     return (m + n - 1) / n;
 }
 
-template <int qk, int qi, typename block_q_t, int vdr, vec_dot_cute_sycl_t vec_dot_cute_sycl>
+// TODO: create a traits struct for qk, qi, qr
+template <size_t rows_per_sg, int qk, int qi, int qr, typename block_q_t, int vdr,
+          vec_dot_cute_sycl_t vec_dot_cute_sycl>
 static void mul_mat_vec_ocl(const void * __restrict__ vx, const void * __restrict__ vy, float * __restrict__ dst,
-                            const int ncols, const int nrows, const sycl::nd_item<3> & nd_item) {
-    const auto       sg           = nd_item.get_sub_group();
-    const int        sg_range     = sg.get_group_linear_range();
-    const int        workgroup_id = nd_item.get_group_linear_id();
-    const int        sg_id        = sg.get_group_linear_id();
-    const int        sg_global_id = workgroup_id * sg_range + sg_id;
-    constexpr size_t sgs_per_row  = 1;  // TODO: Adjust appropriately
-    const int        row          = safe_div(sg_global_id, sgs_per_row);
+                            const size_t ncols, const size_t nrows, const sycl::nd_item<3> & nd_item) {
+    const auto   sg           = nd_item.get_sub_group();
+    const int    sg_range     = sg.get_group_linear_range();
+    const int    workgroup_id = nd_item.get_group_linear_id();
+    const int    sg_id        = sg.get_group_linear_id();
+    const int    sg_global_id = workgroup_id * sg_range + sg_id;
+    // TODO: Considering more than one sg per row maybe?
+    // constexpr size_t sgs_per_row  = 1;
+    // const size_t     base_row     = rows_per_sg * safe_div(sg_global_id, sgs_per_row);
+    const size_t base_row     = rows_per_sg * sg_global_id;
 
-    if (row >= nrows) {
+    if (base_row >= nrows) {
         return;
     }
 
     const size_t     blocks_per_row      = ncols / qk;
     constexpr size_t blocks_per_subgroup = safe_div(vdr * WARP_SIZE, qi);  // Ensuring blocks_per_subgroup > 0
-
     constexpr size_t block_elements_per_subgroup = qi / vdr;
 
     assert(blocks_per_subgroup > 0);
     assert(block_elements_per_subgroup > 0);
 
-    // partial sum for each thread
-    float partial_sum = 0.0f;
-
     const block_q_t *  x = (const block_q_t *) vx;
     const block_q8_1 * y = (const block_q8_1 *) vy;
 
-    // TODO: Change this for the actual subgroup id
-    for (size_t i = sg.get_local_linear_id() / block_elements_per_subgroup; i < blocks_per_row;
-         i += blocks_per_subgroup) {
-        const int ibx = row * blocks_per_row + i;  // x block index
+    // TODO: Change this for the actuabase_row l subgroup id
+    for (size_t row = base_row; row < (base_row + rows_per_sg) && row < nrows; row++) {
+        float partial_sum = 0.0f;
+        for (size_t i = sg.get_local_linear_id() / block_elements_per_subgroup; i < blocks_per_row;
+             i += blocks_per_subgroup) {
+            const size_t ibx        = row * blocks_per_row + i;  // x block index
+            // TODO: Generalize offset. Right now only works for quantizations that don't split
+            // in high/low bits (3,5,6)
+            const size_t ibx_offset = ibx * (qk / qr);
+            // ncols / qr -> size of weights in number of bytes
+            const size_t d_offset   = (ncols / qr * nrows) + ibx * sizeof(ggml_half);
 
-        const int iby = i * (qk / QK8_1);          // y block index that aligns with ibx
+            // TODO: Will need adjustment with the Q4_0 blockload
+            const int iby = i * (qk / QK8_1);  // y block index that aligns with ibx
 
-        for (size_t elem = 0; elem < block_elements_per_subgroup; elem += WARP_SIZE) {
-            // x block quant index when casting the quants to int
-            const int iqs = elem + vdr * (sg.get_local_linear_id() % block_elements_per_subgroup);
+            for (size_t elem = 0; elem < block_elements_per_subgroup; elem += WARP_SIZE) {
+                // x block quant index when casting the quants to int
+                const int iqs = elem + vdr * (sg.get_local_linear_id() % block_elements_per_subgroup);
 
-            partial_sum += vec_dot_cute_sycl(&x[ibx], &y[iby], iqs);
-
-            if (sg_global_id == 0) {
-                // cute::print(
-                //     "mul_mat_vec_ocl: tid=%d item<2>=%d local_id<2>=%d row=%d, bpr=%d bps=%d eps=%d i=%d elem=%d "
-                //     "ibx=%d iby=%d "
-                //     "iqs=%d\n",  // partial_sum=%.8f\n",
-                //     nd_item.get_global_linear_id(), nd_item.get_local_id(2), sg.get_local_linear_id(), row,
-                //     blocks_per_row, blocks_per_subgroup, block_elements_per_subgroup, i, elem, ibx, iby,
-                //     iqs /* , partial_sum */);
+                // if (sg_global_id < 4 && nd_item.get_local_id(2) % 1 == 0) {
+                //     // INFO: to have better ganularity on what to print
+                //     for (size_t i = 0; i < nd_item.get_global_range(2); i++) {
+                //         if (cute::thread(i)) {
+                //             cute::print("mul_mat_vec_ocl: ");
+                //             cute::print("tid=%d ", nd_item.get_global_linear_id());
+                //             cute::print("item<2>=%d ", nd_item.get_local_id(2));
+                //             cute::print("local_id<2>=%d ", sg.get_local_linear_id());
+                //             cute::print("row=%d, ", row);
+                //             // cute::print("bpr=%d ", blocks_per_row);
+                //             // cute::print("bps=%d ", blocks_per_subgroup);
+                //             // cute::print("eps=%d ", block_elements_per_subgroup);
+                //             cute::print("i=%d ", i);
+                //             cute::print("elem=%d ", elem);
+                //             cute::print("ibx=%zu ", ibx);
+                //             cute::print("newibx=%zu ", ibx_offset);
+                //             // cute::print("iby=%d ", iby);
+                //             cute::print("iqs=%d", iqs);
+                //             cute::print("\n");
+                //         }
+                //     }
+                // }
+                partial_sum += vec_dot_cute_sycl(x, &y[iby], iqs, ibx_offset, d_offset);
             }
         }
-    }
 
-    auto sum = sycl::reduce_over_group(nd_item.get_sub_group(), partial_sum, std::plus<>());
-
-    if (sg.leader()) {
-        dst[row] = sum;
+        auto sum = sycl::reduce_over_group(nd_item.get_sub_group(), partial_sum, std::plus<>());
+        if (sg.leader()) {
+            dst[row] = sum;
+        }
     }
 }
 
@@ -130,12 +137,6 @@ static void mul_mat_vec_cute(/* Tensor_VX tensor_vx, */ const void * vx, /* Tens
                              float * /* TensorDST */ dst, const int ncols, const int nrows,
                              const sycl::nd_item<3> & nd_item) {
     const int row = nd_item.get_group(2) * nd_item.get_local_range(1) + nd_item.get_local_id(1);
-
-    if (nd_item.get_local_id(2) == 0) {
-        cute::print("mul_mat_vec_cute: tid=%d item<2>=%d row=%d\n" /* partial_sum=%.8f\n" */,
-                    nd_item.get_global_linear_id(), nd_item.get_local_id(2), row);
-    }
-    syncthreads();
 
     if (row >= nrows) {
         return;
@@ -174,23 +175,23 @@ static void mul_mat_vec_cute(/* Tensor_VX tensor_vx, */ const void * vx, /* Tens
 
     // clang-format off
 #if 0
-    if (cute::thread0()) {
-        cute::print("tensor_vx: "); cute::print(tensor_vx); cute::print("\n");
-        cute::print("thr_copy_A: "); cute::print(thr_copy_A); cute::print("\n");
-        // cute::print("cute_tensor_vy: "); cute::print(cute_tensor_vy); cute::print("\n");
-        // cute::print("cute_tensor_dst: "); cute::print(cute_tensor_dst); cute::print("\n");
-        cute::print("thr_copy_A: "); cute::print(thr_copy_A); cute::print("\n");
-        // cute::print("blk_load_s: "); cute::print(blk_load_s); cute::print("\n");
-        // cute::print("thread_s: "); cute::print(thread_s); cute::print("\n");
-        cute::print("\n\n");
-    }
+    // if (cute::thread0()) {
+    //     cute::print("tensor_vx: "); cute::print(tensor_vx); cute::print("\n");
+    //     cute::print("thr_copy_A: "); cute::print(thr_copy_A); cute::print("\n");
+    //     // cute::print("cute_tensor_vy: "); cute::print(cute_tensor_vy); cute::print("\n");
+    //     // cute::print("cute_tensor_dst: "); cute::print(cute_tensor_dst); cute::print("\n");
+    //     cute::print("thr_copy_A: "); cute::print(thr_copy_A); cute::print("\n");
+    //     // cute::print("blk_load_s: "); cute::print(blk_load_s); cute::print("\n");
+    //     // cute::print("thread_s: "); cute::print(thread_s); cute::print("\n");
+    //     cute::print("\n\n");
+    // }
     syncthreads();
-    if (cute::thread(1 * WARP_SIZE)) {
-        cute::print("tensor_vx: "); cute::print(tensor_vx); cute::print("\n");
-        cute::print("thr_copy_A: "); cute::print(thr_copy_A); cute::print("\n");
-        // cute::print("cute_tensor_vy: "); cute::print(cute_tensor_vy); cute::print("\n");
-        // cute::print("cute_tensor_dst: "); cute::print(cute_tensor_dst); cute::print("\n");
-    }
+    // if (cute::thread(1 * WARP_SIZE)) {
+    //     cute::print("tensor_vx: "); cute::print(tensor_vx); cute::print("\n");
+    //     cute::print("thr_copy_A: "); cute::print(thr_copy_A); cute::print("\n");
+    //     // cute::print("cute_tensor_vy: "); cute::print(cute_tensor_vy); cute::print("\n");
+    //     // cute::print("cute_tensor_dst: "); cute::print(cute_tensor_dst); cute::print("\n");
+    // }
 #endif
     // clang-format on
 
@@ -233,6 +234,7 @@ static __dpct_inline__ float vec_dot_q4_0_q8_1_impl(const int * v, const int * u
                                                     const sycl::half2 & ds8) {
     int sumi = 0;
 
+    constexpr size_t VDR_Q4_0_Q8_1_MMVCUTE = quants::reordered::block_q4_0::traits::vdr_q8_1;
 #pragma unroll
     for (size_t i = 0; i < VDR_Q4_0_Q8_1_MMVCUTE; ++i) {
         const int vi0 = (v[i] >> 0) & 0x0F0F0F0F;
@@ -250,41 +252,48 @@ static __dpct_inline__ float vec_dot_q4_0_q8_1_impl(const int * v, const int * u
 }
 
 static __dpct_inline__ float vec_dot_q4_0_q8_1(const void * __restrict__ vbq, const block_q8_1 * __restrict__ bq8_1,
-                                               const int & iqs) {
-    const block_q4_0 * bq4_0 = (const block_q4_0 *) vbq;
-    int                v[VDR_Q4_0_Q8_1_MMVCUTE];
-
+                                               const int & iqs, const int ibx_offset, const int d_offset) {
 #ifdef __SYCL_DEVICE_ONLY__
-    vector_t<int, 2> coord = { iqs, 0 };
-    *reinterpret_cast<vector_t<ushort, 2> *>(v) =
-        // u8 -> atomic value to bytes
-        // m1 -> vertical dimension
-        // k32 -> k-dimension size (also deifnes the stride of the values received by threads)
-        // v2 -> 2 values
-        // This translates to 64 bytes loaded, in two pairs of two bytes because:
-        //   k32 * v2 = 64 u8 values,
-        //   v2 = two blocks loaded 64 / 2 -> 32 u8 values are loaded per block,
-        //   32 / WARP_SIZE = 2 values per thread
-        detail::__builtin_IB_subgroup_block_read_flat_u8_m1k32v2((long) (vbq), 512 * sizeof(uint8_t) - 1, 16 - 1,
-                                                                 512 * sizeof(uint8_t) - 1, coord);
+    // vector_t<int, 2> coord = { iqs, 0 };
+    // *reinterpret_cast<vector_t<ushort, 2> *>(v) =
+    // u8 -> atomic value to bytes
+    // m1 -> vertical dimension
+    // k32 -> k-dimension size (also deifnes the stride of the values received by threads)
+    // v2 -> 2 values
+    // This translates to 64 bytes loaded, in two pairs of two bytes because:
+    //   k32 * v2 = 64 u8 values,
+    //   v2 = two blocks loaded 64 / 2 -> 32 u8 values are loaded per block,
+    //   32 / WARP_SIZE = 2 values per thread
+    // detail::__builtin_IB_subgroup_block_read_flat_u8_m1k32v2((long) (vbq), 512 * sizeof(uint8_t) - 1, 16 - 1,
+    //                                                          512 * sizeof(uint8_t) - 1, coord);
+
     // baseoffset ALWAYS BEGINNING OF MEM REGION
-    // Dimensions are defined by builtin (1k32v2) 1 x (32 * 2)
+    // Dimensions are defined by builtin (1k16v1) 1 x (16 * 1)
     // width (bytes)     -> actual width in bytes of the memory region
     // height (elements) -> 1
     // pitch (bytes)     -> helps defining the subblock of memory to access
     // x,y coord like system that helps identifying the block to load. Row major.
+    // detail::__builtin_IB_subgroup_block_read_flat_u32_m1k16v1
 #endif
 
-    int u[2 * VDR_Q4_0_Q8_1_MMVCUTE];  // q8 bytes == 2 * q4 bytes
+    constexpr size_t VDR_Q4_0_Q8_1_MMVCUTE = quants::reordered::block_q4_0::traits::vdr_q8_1;
+    const uint8_t *  bq4_0                 = static_cast<const uint8_t *>(vbq) + ibx_offset;
+    const ggml_half  d = *(reinterpret_cast<const ggml_half *>(static_cast<const uint8_t *>(vbq) + d_offset));
+    int              v[VDR_Q4_0_Q8_1_MMVCUTE];
+    int              u[2 * VDR_Q4_0_Q8_1_MMVCUTE];  // q8 bytes == 2 * q4 bytes
 
 #pragma unroll
+
     for (size_t i = 0; i < VDR_Q4_0_Q8_1_MMVCUTE; ++i) {
-        v[i]         = get_int_from_uint8(bq4_0->qs, iqs + i);         // A
+        v[i]         = get_int_from_uint8(bq4_0, iqs + i);             // A
         u[2 * i + 0] = get_int_from_int8_aligned(bq8_1->qs, iqs + i);  // B
         u[2 * i + 1] = get_int_from_int8_aligned(bq8_1->qs, iqs + i + QI4_0);
     }
 
-    return vec_dot_q4_0_q8_1_impl(v, u, bq4_0->d, bq8_1->ds);
+    // cute::print("ibx_offset=%zu, d_offset=%zu, v[0]=%d, v[1]=%d, d=%.8f\n", ibx_offset, d_offset, v[0], v[1],
+    //             static_cast<float>(d));
+
+    return vec_dot_q4_0_q8_1_impl(v, u, d, bq8_1->ds);
 }
 
 // NOTE: Will only work with GGML_SYCL_DISABLE_OPT=1 for now
@@ -294,9 +303,10 @@ static void mul_mat_vec_cute_q4_0_q8_1_sycl(const void * vx, const void * vy, fl
     // TODO: What is the purpose of GGML_SYCL_MMV_Y
     const int        block_num_y    = safe_div(nrows, GGML_SYCL_MMV_Y);
     constexpr size_t cute_sg_per_wg = 16;
+    constexpr size_t rows_per_sg    = 1;
     GGML_ASSERT(block_num_y % cute_sg_per_wg == 0);
 
-    const sycl::range<3> global_size(1, GGML_SYCL_MMV_Y, block_num_y * WARP_SIZE);
+    const sycl::range<3> global_size(1, GGML_SYCL_MMV_Y, (block_num_y * WARP_SIZE) / rows_per_sg);
     const sycl::range<3> wg_size(1, GGML_SYCL_MMV_Y, cute_sg_per_wg * WARP_SIZE);
 
     // TODO: It may be simpler to switch around the shape and set it to LayoutLeft
@@ -308,19 +318,21 @@ static void mul_mat_vec_cute_q4_0_q8_1_sycl(const void * vx, const void * vy, fl
     //                                         cute::make_layout(cute::select<1, 0>(vec_dot_shape), cute::LayoutRight{}));
     // auto cute_tensor_dst =
     //     cute::make_tensor(dst, cute::make_layout(cute::select<2, 0>(vec_dot_shape), cute::LayoutRight{}));
-    printf("nrows=%d, ncols=%d\n", nrows, ncols);
-    printf("global_size=%zu,%zu,%zu, local_size=%zu,%zu,%zu\n", global_size[0], global_size[1], global_size[2],
-           wg_size[0], wg_size[1], wg_size[2]);
+    // printf("nrows=%d, ncols=%d\n", nrows, ncols);
+    // printf("global_size=%zu,%zu,%zu, local_size=%zu,%zu,%zu\n", global_size[0], global_size[1], global_size[2],
+    //        wg_size[0], wg_size[1], wg_size[2]);
 
+    constexpr size_t VDR_Q4_0_Q8_1_MMVCUTE = quants::reordered::block_q4_0::traits::vdr_q8_1;
     stream->submit([&](sycl::handler & cgh) {
-        cgh.parallel_for(sycl::nd_range<3>(global_size, wg_size),
-                         [=](sycl::nd_item<3> nd_item) [[sycl::reqd_sub_group_size(WARP_SIZE)]] {
-                             // mul_mat_vec_cute<QK4_0, QI4_0, block_q4_0, VDR_Q4_0_Q8_1_MMVCUTE, vec_dot_q4_0_q8_1>(
-                             //     /* cute_tensor_vx, */ vx, vy, dst, ncols, nrows, nd_item);
+        cgh.parallel_for(
+            sycl::nd_range<3>(global_size, wg_size),
+            [=](sycl::nd_item<3> nd_item) [[sycl::reqd_sub_group_size(WARP_SIZE)]] {
+                // mul_mat_vec_cute<QK4_0, QI4_0, block_q4_0, VDR_Q4_0_Q8_1_MMVCUTE, vec_dot_q4_0_q8_1>(
+                //     /* cute_tensor_vx, */ vx, vy, dst, ncols, nrows, nd_item);
 
-                             mul_mat_vec_ocl<QK4_0, QI4_0, block_q4_0, VDR_Q4_0_Q8_1_MMVCUTE, vec_dot_q4_0_q8_1>(
-                                 vx, vy, dst, ncols, nrows, nd_item);
-                         });
+                mul_mat_vec_ocl<rows_per_sg, QK4_0, QI4_0, QR4_0, block_q4_0, VDR_Q4_0_Q8_1_MMVCUTE, vec_dot_q4_0_q8_1>(
+                    vx, vy, dst, ncols, nrows, nd_item);
+            });
     });
 }
 
--- ggml/src/ggml-sycl/mmvcute.hpp
+++ ggml/src/ggml-sycl/mmvcute.hpp
@@ -11,13 +11,55 @@
 #include "common.hpp"
 
 typedef float (*vec_dot_cute_sycl_t)(const void * __restrict__ vbq, const block_q8_1 * __restrict__ bq8_1,
-                                     const int & iqs);
+                                     const int & iqs, const int ibx_offset, const int d_offset);
 
 // VDR = vec dot ratio, how many contiguous integers each thread processes when the vec dot kernel is called
 // Defined by how the vec dot algorithm is written
 constexpr size_t VDR_Q6_K_Q8_1_MMVCUTE = 1;
 constexpr size_t VDR_Q4_K_Q8_1_MMVCUTE = 2;
-constexpr size_t VDR_Q4_0_Q8_1_MMVCUTE = 2;
+
+#ifdef __SYCL_DEVICE_ONLY__
+template <class T, int N> using vector_t = T __attribute__((ext_vector_type(N)));
+#else
+template <class T, int N> using vector_t = sycl::marray<T, N>;
+#endif
+
+#ifdef __SYCL_DEVICE_ONLY__
+namespace detail {
+SYCL_EXTERNAL extern "C" vector_t<ushort, 2> __builtin_IB_subgroup_block_read_flat_u8_m1k32v2(
+    long baseoffset, int width_minus_one, int height_minus_one, int pitch_minus_one, vector_t<int, 2> coord);
+SYCL_EXTERNAL extern "C" uint32_t __builtin_IB_subgroup_block_read_flat_u32_m1k16v1(
+    long baseoffset, int width_minus_one, int height_minus_one, int pitch_minus_one, vector_t<int, 2> coord);
+}  // namespace detail
+#endif
+
+namespace quants {
+namespace reordered {
+namespace detail {
+
+struct block_q4_0_traits {};
+
+}  // namespace detail
+
+// Expected
+// ncols * nrows / QK4_0          blocks and scales
+// ncols * nrows * QK4_0          quants
+// ncols * nrows * QK4_0 / QR4_0  bytes
+struct block_q4_0 {
+    struct traits {
+        static constexpr size_t qk       = QK4_0;
+        static constexpr size_t qi       = QI4_0;
+        static constexpr size_t qr       = QR4_0;
+        static constexpr size_t vdr_q8_1 = 2;
+    };
+
+    // qs and d are contiguous in memory, out-of-bounds qs will access to d values
+    uint8_t *   qs;
+    ggml_half * d;
+};
+
+}  // namespace reordered
+}  // namespace quants
 
 void ggml_sycl_op_mul_mat_vec_cute(ggml_backend_sycl_context & ctx, const ggml_tensor * src0, const ggml_tensor * src1,
                                    ggml_tensor * dst, const char * src0_dd_i, const float * src1_ddf_i,
